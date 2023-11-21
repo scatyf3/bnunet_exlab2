386faf78022c156501e030bfaafdade3ac6413e8
scat <suzuki1968@protonmail.com>
2023-11-21 11:40:19 +0800

    完成两数之和

--- routes/api.js
+++ routes/api.js
@@ -10,4 +10,13 @@ router.get('/echo', function(req, res, next) {
   res.send({ 'name': req.query.name });
 });
 
+router.get('/sum', function(req, res, next) {
+  //手动置顶类型为int，否则就是字符串连缀了😭
+  const num_a = parseInt(req.query.a);
+  const num_b = parseInt(req.query.b);
+  res.send({ 'result': num_a + num_b });
+});
+
+
+
 module.exports = router;
